de5ae952e8df37d2bc4159a9e835a306bd13f429
Valid Code <thevalidcode@gmail.com>
2025-07-14 09:14:55 +0100

    fix: updated build for mac

--- validflow-backend/build-backend.py
+++ validflow-backend/build-backend.py
@@ -1,8 +1,8 @@
 import PyInstaller.__main__
 
 PyInstaller.__main__.run([
-    "start.py",
     "--onefile",
     "--distpath", "../validflow-frontend/src-tauri/bin",
-    "--name", "start"
+    "--name", "start",
+    "start.py"
 ])
